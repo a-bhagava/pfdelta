f89f793f4c70d0fe18d2cd776a96600136007df2
James <jamesyc@mit.edu>
2026-08-25 17:29:18 -0400

    idk how git works

---
+++ core/configs/canos_task_3_1_finetune/canos_task_3_1_finetune_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -p mit_normal_gpu,mit_preemptable
+#SBATCH --requeue
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:l40s:1
+#SBATCH --output=canos_hyperparam_finetune_%j.out
+#SBATCH --signal=USR1@90
+#SBATCH --time=05:59:00
+
+source ~/.bashrc
+conda activate pfdelta2
+exec python main.py --config canos_task_3_1_finetune/canos_task_3_1_finetune_0
